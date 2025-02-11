7155e2f2fb257af03bfda6fae370a6cf6faba8f5
ascpixi <44982772+ascpixi@users.noreply.github.com>
2025-02-12 00:50:46 +0100

    fix: create sprintf_s implementation

--- src/coreclr/jit/libryujit/polyfill.cpp
+++ src/coreclr/jit/libryujit/polyfill.cpp
@@ -1,7 +1,8 @@
 #include <stdarg.h>
 
 #include "./host.h"
-#include "./lib/debug-trap.h"  
+#include "./lib/debug-trap.h"
+#include "./lib/nanoprintf.h"
 
 int vflogf(FILE* file, const char* fmt, va_list args);
 
@@ -13,4 +14,14 @@ void __ryujit_pal_asserte_fail(const char* fmt, ...)
     va_end(args);
 
     psnip_trap();
+}
+
+int sprintf_s(char* string, size_t sizeInBytes, const char* format, ...)
+{
+    int     ret;
+    va_list arglist;
+    va_start(arglist, format);
+    ret = npf_vsnprintf(string, sizeInBytes, format, arglist);
+    va_end(arglist);
+    return ret;
 }
